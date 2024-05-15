ed8265bd259f5ce6134873cf1648fcdc03b26a97
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-05-15 16:31:53 +0200

    fix: test pipeline

--- app-backend/config.js
+++ app-backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo:27017/weather?directConnection=true',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/weather?directConnection=true',
     MONGO_URI_TEST: process.env.MONGO_URI_TEST || 'mongodb://localhost:27017/',
     CORS_ORIGIN: 'http://localhost:5173',
     SERVER_URL: 'http://localhost:3000',
